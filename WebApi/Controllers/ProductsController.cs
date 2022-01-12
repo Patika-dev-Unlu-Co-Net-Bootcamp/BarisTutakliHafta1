fbe54d2d0247c9a67a87914cfa53a2c2b02b8dab
kodluyoruz <baristutakli@hotmail.com>
2022-01-12 08:29:37 +0300

    Created controllers and entities

---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,89 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            
+            return Ok(InMemoryDal.MemoryDal.productList);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            Product result = InMemoryDal.MemoryDal.productList.SingleOrDefault(p => p.Id == id);
+            if (result !=null)
+            {
+                return Ok(result);
+            }
+            return NoContent();// return 204 
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody]Product product)
+        {
+            bool check = true;
+            InMemoryDal.MemoryDal.productList.ForEach(p =>
+            {
+                if (p.Id == product.Id)
+                {
+                     check = false;
+                }
+            });
+            if (!check)
+            {
+                return BadRequest();
+            }
+            return Created("",new { time = DateTime.Now });
+            
+        }
+        [HttpPut]
+        public IActionResult Update([FromBody] Product product)
+        {
+            bool check = true;
+            InMemoryDal.MemoryDal.productList.ForEach(p =>
+            {
+                if (p.Id == product.Id)
+                {
+                    p.ProductName = product.ProductName;
+                    p.CategoryId = product.CategoryId;
+                    p.PublishingDate = product.PublishingDate;
+                }
+            });
+            if (!check)
+            {
+                return NotFound();
+            }
+            return Ok();
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            bool check = true;
+            InMemoryDal.MemoryDal.productList.ForEach(p =>
+            {
+                if (p.Id == id)
+                {
+                    InMemoryDal.MemoryDal.productList.Remove(p);
+                }
+            });
+            if (!check)
+            {
+                return NotFound();
+            }
+            return Ok();
+
+        }
+    }
+}
---
+++ WebApi/InMemoryDal/MemoryDal.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.InMemoryDal
+{
+    public class MemoryDal
+    {
+        public static List<Product> productList = new List<Product> {
+                new Product { Id=1, CategoryId=2, ProductName="Watch", PublishingDate=DateTime.Now },
+                new Product { Id=2, CategoryId=3, ProductName="Phone", PublishingDate=DateTime.Now },
+                new Product { Id=3, CategoryId=4, ProductName="Tablet", PublishingDate=DateTime.Now }
+            };
+    }
+}
